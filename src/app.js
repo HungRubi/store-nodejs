92b2e8ba79f71a723b70e332fec1b1c3ab9de528
unknown <huyhung18042002@gmail.com>
2024-06-28 23:14:24 +0700

    update 2

--- src/app.js
+++ src/app.js
@@ -2,24 +2,31 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const handlebars = require('express-handlebars');
-const port = 3001;
+const port = 3000;
 
 const app = express();
 
 //http logger
-app.use(morgan('combined'));
-
-//static file
-app.use(express.static(path.join(__dirname,"public")));
+// app.use(morgan('combined'));
 
 //template engine
 app.engine('.hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname,'resources', 'views'));
 
-
+//static file
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req, res) => {
     res.render('home');
+})
+app.get('/search',(req, res) => {
+    console.log(req.query.age);
+    res.render('search');
+})
+
+app.get('/news', (req,res) => {
+    res.render('news');
+    console.log(req.query.q);
 })
 app.listen(port,() => console.log(`Server is running localhost:${port}`));
