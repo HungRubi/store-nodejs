5af59d701f514b03c310080e101b400242e03bde
unknown <huyhung18042002@gmail.com>
2024-07-02 00:50:42 +0700

    husky

--- src/resources/routes/site.route.js
+++ src/resources/routes/site.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const siteController = require('../app/controllers/site.controller');
 
-router.use('/search', siteController.search);
+    router.use('/search', siteController.search);
 router.use('/', siteController.index);
 
-module.exports = router;
+    module.exports = router;
